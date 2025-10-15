77bd8ff11ca5ec68484df52c89dccab4483f5f03
unknown <mabborangbryan3@gmail.com>
2025-10-15 12:08:40 +0800

    fixed conflicts

--- PAKOPointOfSale/Categories/ArchiveCategory.Designer.cs
+++ PAKOPointOfSale/Categories/ArchiveCategory.Designer.cs
@@ -40,18 +40,22 @@
             // 
             // btnYes
             // 
-            btnYes.Location = new Point(171, 251);
+            btnYes.BackColor = Color.Green;
+            btnYes.FlatStyle = FlatStyle.Popup;
+            btnYes.ForeColor = Color.Black;
+            btnYes.Location = new Point(307, 222);
             btnYes.Name = "btnYes";
             btnYes.Size = new Size(75, 23);
             btnYes.TabIndex = 13;
             btnYes.Text = "Yes";
-            btnYes.UseVisualStyleBackColor = true;
+            btnYes.UseVisualStyleBackColor = false;
             btnYes.Click += btnYes_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(15, 111);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(18, 95);
             label3.Name = "label3";
             label3.Size = new Size(67, 15);
             label3.TabIndex = 12;
@@ -60,7 +64,8 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 70);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(15, 54);
             label2.Name = "label2";
             label2.Size = new Size(90, 15);
             label2.TabIndex = 11;
@@ -69,42 +74,48 @@
             // txtDescription
             // 
             txtDescription.Enabled = false;
-            txtDescription.Location = new Point(140, 107);
+            txtDescription.Location = new Point(126, 92);
             txtDescription.Multiline = true;
             txtDescription.Name = "txtDescription";
-            txtDescription.Size = new Size(197, 117);
+            txtDescription.Size = new Size(337, 117);
             txtDescription.TabIndex = 9;
             // 
             // txtCategoryName
             // 
             txtCategoryName.Enabled = false;
-            txtCategoryName.Location = new Point(140, 66);
+            txtCategoryName.Location = new Point(126, 51);
             txtCategoryName.Name = "txtCategoryName";
-            txtCategoryName.Size = new Size(197, 23);
+            txtCategoryName.Size = new Size(337, 23);
             txtCategoryName.TabIndex = 8;
             // 
             // btnNo
             // 
-            btnNo.Location = new Point(261, 251);
+            btnNo.BackColor = Color.Red;
+            btnNo.FlatStyle = FlatStyle.Popup;
+            btnNo.ForeColor = Color.Black;
+            btnNo.Location = new Point(388, 221);
             btnNo.Name = "btnNo";
             btnNo.Size = new Size(75, 23);
             btnNo.TabIndex = 14;
             btnNo.Text = "No";
-            btnNo.UseVisualStyleBackColor = true;
+            btnNo.UseVisualStyleBackColor = false;
             btnNo.Click += btnNo_Click;
             // 
             // btnArchive
             // 
-            btnArchive.Location = new Point(63, 210);
+            btnArchive.BackColor = Color.Orange;
+            btnArchive.FlatStyle = FlatStyle.Popup;
+            btnArchive.Location = new Point(226, 222);
             btnArchive.Name = "btnArchive";
             btnArchive.Size = new Size(75, 23);
             btnArchive.TabIndex = 15;
             btnArchive.Text = "Archive";
-            btnArchive.UseVisualStyleBackColor = true;
+            btnArchive.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.ForeColor = Color.Transparent;
             label1.Location = new Point(15, 19);
             label1.Name = "label1";
             label1.Size = new Size(256, 15);
@@ -115,7 +126,9 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.MenuText;
             ClientSize = new Size(533, 273);
+            ControlBox = false;
             Controls.Add(label1);
             Controls.Add(btnArchive);
             Controls.Add(btnNo);
@@ -125,6 +138,7 @@
             Controls.Add(txtDescription);
             Controls.Add(txtCategoryName);
             Name = "ArchiveCategory";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Archive";
             Load += Archive_Load;
             ResumeLayout(false);
--- PAKOPointOfSale/Categories/CategoriesList.Designer.cs
+++ PAKOPointOfSale/Categories/CategoriesList.Designer.cs
@@ -35,17 +35,17 @@ namespace PAKOPointOfSale.Categories
             dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
             description = new DataGridViewTextBoxColumn();
             is_active = new DataGridViewTextBoxColumn();
-            edit = new DataGridViewButtonColumn();
-            archive = new DataGridViewButtonColumn();
             categoryBindingSource1 = new BindingSource(components);
             categoryBindingSource = new BindingSource(components);
             btnAdd = new Button();
             btnPrint = new Button();
             txtSearch = new TextBox();
             label1 = new Label();
+            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)categoryBindingSource1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)categoryBindingSource).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
@@ -54,12 +54,12 @@ namespace PAKOPointOfSale.Categories
             dataGridView1.AllowUserToDeleteRows = false;
             dataGridView1.AutoGenerateColumns = false;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { id, dataGridViewTextBoxColumn2, description, is_active, edit, archive });
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { id, dataGridViewTextBoxColumn2, description, is_active });
             dataGridView1.DataSource = categoryBindingSource1;
-            dataGridView1.Location = new Point(12, 44);
+            dataGridView1.Location = new Point(10, 107);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.ReadOnly = true;
-            dataGridView1.Size = new Size(644, 379);
+            dataGridView1.Size = new Size(644, 343);
             dataGridView1.TabIndex = 0;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick_1;
             // 
@@ -77,6 +77,7 @@ namespace PAKOPointOfSale.Categories
             dataGridViewTextBoxColumn2.HeaderText = "Category Name";
             dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
             dataGridViewTextBoxColumn2.ReadOnly = true;
+            dataGridViewTextBoxColumn2.Width = 150;
             // 
             // description
             // 
@@ -96,31 +97,13 @@ namespace PAKOPointOfSale.Categories
             is_active.Resizable = DataGridViewTriState.True;
             is_active.SortMode = DataGridViewColumnSortMode.NotSortable;
             // 
-            // edit
-            // 
-            edit.DataPropertyName = "id";
-            edit.HeaderText = "";
-            edit.Name = "edit";
-            edit.ReadOnly = true;
-            edit.Text = "Edit";
-            edit.UseColumnTextForButtonValue = true;
-            // 
-            // archive
-            // 
-            archive.DataPropertyName = "id";
-            archive.HeaderText = "";
-            archive.Name = "archive";
-            archive.ReadOnly = true;
-            archive.Text = "Archive";
-            archive.UseColumnTextForButtonValue = true;
-            // 
             // categoryBindingSource1
             // 
             categoryBindingSource1.DataSource = typeof(Category);
             // 
             // btnAdd
             // 
-            btnAdd.Location = new Point(500, 14);
+            btnAdd.Location = new Point(471, 78);
             btnAdd.Name = "btnAdd";
             btnAdd.Size = new Size(75, 23);
             btnAdd.TabIndex = 1;
@@ -130,7 +113,7 @@ namespace PAKOPointOfSale.Categories
             // 
             // btnPrint
             // 
-            btnPrint.Location = new Point(581, 14);
+            btnPrint.Location = new Point(552, 78);
             btnPrint.Name = "btnPrint";
             btnPrint.Size = new Size(75, 23);
             btnPrint.TabIndex = 3;
@@ -139,7 +122,7 @@ namespace PAKOPointOfSale.Categories
             // 
             // txtSearch
             // 
-            txtSearch.Location = new Point(60, 15);
+            txtSearch.Location = new Point(63, 79);
             txtSearch.Name = "txtSearch";
             txtSearch.Size = new Size(366, 23);
             txtSearch.TabIndex = 7;
@@ -148,17 +131,27 @@ namespace PAKOPointOfSale.Categories
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 20);
+            label1.Location = new Point(15, 86);
             label1.Name = "label1";
-            label1.Size = new Size(42, 15);
+            label1.Size = new Size(45, 15);
             label1.TabIndex = 8;
-            label1.Text = "Search";
+            label1.Text = "Search:";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.DimGray;
+            pictureBox2.Location = new Point(1, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(662, 57);
+            pictureBox2.TabIndex = 20;
+            pictureBox2.TabStop = false;
             // 
             // CategoriesList
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(672, 417);
+            ClientSize = new Size(661, 450);
+            Controls.Add(pictureBox2);
             Controls.Add(label1);
             Controls.Add(txtSearch);
             Controls.Add(btnPrint);
@@ -170,6 +163,7 @@ namespace PAKOPointOfSale.Categories
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ((System.ComponentModel.ISupportInitialize)categoryBindingSource1).EndInit();
             ((System.ComponentModel.ISupportInitialize)categoryBindingSource).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -196,5 +190,6 @@ namespace PAKOPointOfSale.Categories
         private DataGridViewTextBoxColumn is_active;
         private DataGridViewButtonColumn edit;
         private DataGridViewButtonColumn archive;
+        private PictureBox pictureBox2;
     }
 }
